707836f3c6001dafc90a9ccd8714416b119d72d2
kayace <adnankayace@gmail.com>
2018-04-29 18:25:44 +0300

    second commit by adnan

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,5 +1,5 @@
 # external
-from flask import Flask
+from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -25,6 +25,19 @@ def create_app(config_name):
     migrate = Migrate(app, db)
 
 
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page not found'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'), 500
+
+
     from myapp import models
 
     from .admin import admin as admin_blueprint
@@ -35,6 +48,11 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
+
+    #temporary
+    # @app.route('/500')
+    # def error():
+    #     abort(500)
 
 
     return app
